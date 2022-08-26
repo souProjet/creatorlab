2a63f6213886bde176c9f810c97dc86b48a72f6d
sou - rufus <soubougouin@gmail.com>
2022-08-26 12:47:07 +0200

    create amelia webhook

--- .github/sendwebhook.js
+++ .github/sendwebhook.js
@@ -3,15 +3,31 @@ class Webhook {
     constructor(url) {
         this.url = url;
     }
-    send(message) {
+    send(message, amelia = false) {
         fetch(this.url, {
             "method": "POST",
-            "headers": { "content-type": "application/json" },
+            "headers": {
+                "content-type": "application/json",
+                "key": amelia ? amelia : null
+            },
             "body": JSON.stringify(message)
         })
     }
 }
+
+// DISCORD WEBHOOK
 const webhook = new Webhook(process.argv[2]);
 webhook.send({
     "content": "Déploiement de Creatorlab terminé !"
 });
+
+//AMELIA WEBHOOK
+const amelia = new Webhook(process.argv[3]);
+amelia.send({
+        "action": "send",
+        "desti": process.argv[4],
+        "type": "text",
+        "message": "Déploiement de Creatorlab terminé !"
+    },
+    process.argv[5]
+)

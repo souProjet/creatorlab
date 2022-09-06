ea3b3f7771259203430e20cb781cc0b651ddbea9
sou - rufus <soubougouin@gmail.com>
2022-09-06 19:35:32 +0200

    opti loginToFranceConnect

--- modules/login.js
+++ modules/login.js
@@ -43,8 +43,9 @@ let Login = class Login {
         });
     }
     loginToFranceConnect(page, counter, username, password) {
-        try {
-            return new Promise(async(resolve, reject) => {
+        return new Promise(async(resolve, reject) => {
+            try {
+
                 // if (counter == 3) {
                 //     reject({
                 //         status: false,
@@ -53,56 +54,71 @@ let Login = class Login {
                 // }
                 await page.goto('https://www.e-lyco.fr/');
                 await page.waitForTimeout(2000);
-                await page.$eval('.menu > li > a', el => el.click());
-                await page.waitForTimeout(1000);
-                await page.$eval('.champ', el => el.click());
-                await page.waitForTimeout(500);
-                await page.$eval('#valider', el => el.click());
-                await page.waitForTimeout(1000);
-                await page.waitForSelector('#bouton_eleve', { visible: true });
-                await page.$eval('#bouton_eleve', el => el.click());
-                await page.$eval('#username', (el, username) => el.value = username, username);
-                await page.$eval('#password', (el, password) => el.value = password, password);
-                await page.click('#bouton_valider');
-                let shibsession = '';
-                if (counter != 3) {
-                    //écouter la réponse de la requête POST
-                    let response = await page.waitForResponse(response => response.url());
-
-                    shibsession = response.headers()['set-cookie'] ? response.headers()['set-cookie'].split(';')[0] : '';
-                    if (shibsession.indexOf("_shibsession_") == -1) {
-                        return this.loginToFranceConnect(page, counter + 1, username, password);
+                //await page.$eval('.menu > li > a', el => el.click());
+                let isSuccess = await page.evaluate(() => {
+                    if (document.querySelector('.menu > li > a')) {
+                        document.querySelector('.menu > li > a').click();
+                        return true;
+                    } else {
+                        return false
                     }
-                }
-                await page.waitForTimeout(2000);
-
-                if (await page.url() == 'https://educonnect.education.gouv.fr/idp/profile/SAML2/Redirect/SSO?execution=e1s2') {
-                    //les identifiants sont incorrects
+                });
+                if (!isSuccess) {
                     reject({
                         status: false,
-                        message: 'Identifiants incorrects'
+                        message: 'Une erreur est survenue',
+                        l: 69
                     });
                 } else {
-                    //les identifiants sont corrects
-                    let cookie = await page.cookies();
-                    let sessionId = cookie.find(c => c.name == 'ASP.NET_SessionId').value;
-                    resolve({
-                        status: true,
-                        message: 'Connexion réussie',
-                        sessionId: sessionId,
-                        shibsession: shibsession
-                    });
+                    await page.waitForTimeout(1000);
+                    await page.$eval('.champ', el => el.click());
+                    await page.waitForTimeout(500);
+                    await page.$eval('#valider', el => el.click());
+                    await page.waitForTimeout(1000);
+                    // await page.waitForSelector('#bouton_eleve', { visible: true });
+                    await page.$eval('#bouton_eleve', el => el.click());
+                    await page.$eval('#username', (el, username) => el.value = username, username);
+                    await page.$eval('#password', (el, password) => el.value = password, password);
+                    await page.click('#bouton_valider');
+                    let shibsession = '';
+                    if (counter != 3) {
+                        //écouter la réponse de la requête POST
+                        let response = await page.waitForResponse(response => response.url());
+
+                        shibsession = response.headers()['set-cookie'] ? response.headers()['set-cookie'].split(';')[0] : '';
+                        if (shibsession.indexOf("_shibsession_") == -1) {
+                            return this.loginToFranceConnect(page, counter + 1, username, password);
+                        }
+                    }
+                    await page.waitForTimeout(2000);
+
+                    if (await page.url() == 'https://educonnect.education.gouv.fr/idp/profile/SAML2/Redirect/SSO?execution=e1s2') {
+                        //les identifiants sont incorrects
+                        reject({
+                            status: false,
+                            message: 'Identifiants incorrects'
+                        });
+                    } else {
+                        //les identifiants sont corrects
+                        let cookie = await page.cookies();
+                        let sessionId = cookie.find(c => c.name == 'ASP.NET_SessionId').value;
+                        resolve({
+                            status: true,
+                            message: 'Connexion réussie',
+                            sessionId: sessionId,
+                            shibsession: shibsession
+                        });
+                    }
                 }
+            } catch (err) {
+                //console.error('[CREATOR LAB] Erreur de scraping', err);
+                return {
+                    status: false,
+                    message: 'Une erreur est survenue'
+                };
 
-            });
-        } catch (err) {
-            //console.error('[CREATOR LAB] Erreur de scraping', err);
-            return {
-                status: false,
-                message: 'Une erreur est survenue'
-            };
-
-        }
+            }
+        });
     }
     getUserByUsername(username) {
         //retourne une promise contenant l'utilisateur correspondant à l'username
